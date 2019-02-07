59049d2d20ccf1ba14ef916c845d61b26a2748fe
Utnnapiszti <szymon.halaszuk@gmail.com>
2019-02-07 13:28:31 +0100

    ciag dalszy

--- waves_fun.js
+++ waves_fun.js
@@ -14,8 +14,8 @@ function randomize_vertice_height(){
 	plane.geometry.computeVertexNormals();
 }
 //
-const A = 0.25; // amplitude of wave
-const L = 15; // wavelength
+const A = 0.05; // amplitude of wave
+const L = 10; // wavelength
 const omega = 2*Math.PI/L;
 const g = 9.81; // gravitational force
 const S = 0.2; // speed of wave
@@ -27,38 +27,48 @@ var Len = [];
 var omegs = [];
 var Spee = [];
 var Fee = [];
+var Dd = []
 //	
 for (var i=0; i<5; i++){
-	Amps.push(A*random_height());
-		var Le = L*random_height();
+	Amps.push(A*Math.abs(random_height()));
+		var Le = L*Math.abs(random_height());
 	Len.push(Le);
 	omegs.push(2*Math.PI/Le);
-		var Ss = S*random_height();
+		var Ss = S*Math.abs(random_height());
 	Spee.push(Ss);
 	Fee.push(Ss*2*Le);
+	Dd.push(new THREE.Vector2( Math.abs(random_height()), Math.abs(random_height())/2));
 }
+console.log("\nAmplitudy: "+Amps+"\ndługości fali: "+Len+"\nomegi: "+omegs+"\nspeeds: "+Spee+"\nprzesunięcia fazowe: "+Fee+"\nwiatr: "+Dd);
 //
 function wave_function(){
-	// vertices[3k] = x1,x2..., vertices[3k+1] = y1,y2..., vertices[3k+2] = z1,z2...
+// vertices[3k] = x1,x2..., vertices[3k+1] = y1,y2..., vertices[3k+2] = z1,z2...
 	// var vertices = plane.geometry.attributes.position.array; // powinno być nazewnątrz, żeby nie wyliczać chyba każdego wierzchołka. Bierzemy x i y a potem ustawiamy mu z. To wystarczy dla każdego x, dla każdego y ustaw z(x,y,t)
 	// for (var i=0; i<vertices.length; i+=3){	
-	// }
+// }
 	var t = clock.getElapsedTime(); // time
+	getTotalSurface(t);
 
-	
-//
-	for (x=0; x<vertices.length; x+=3){
-		var state = 0;
-		for (var i=0; i<Amps.length; i++){
-			state = state + getStateOfWave(x,x+1,t,Amps[i],D,omegs[i],Fee[i]);
-		}
-		vertices[x+2] = state;
 
-	}
+
 	plane.geometry.attributes.position.needsUpdate = true;
 	plane.geometry.computeVertexNormals();
 }
-function getStateOfWave(x,y,t,A,D,omega,fi){
+function calculateDerivative(){
+	
+}
+
+function getTotalSurface(t){
+	var surfaceVector = new THREE.Vector3();
+	for (var x=0;x<vertices.length;x+=3){
+		var totalSurface = 0;
+		for (var i=0; i<Amps.length; i++){
+			totalSurface = totalSurface + getStateOfWave(x,x+1,t,Amps[i],Dd[i],omegs[i],Fee[i]);
+		}
+		vertices[x+2] = totalSurface;
+	}
+}
+function getStateOfWave(x,y,t,A,D,omega,fi){ // generate state of wave with xi, yi, ti, Ai, Di, omegai, fii
 	var xy_vec = new THREE.Vector2( x, y );
 	var dotProd = D.dot(xy_vec);
 	var crossProd = dotProd*omega;
